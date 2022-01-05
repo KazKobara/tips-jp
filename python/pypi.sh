ee0452806080fe9d882b7070fbe31ed61dff5ec8
Kaz KOBARA <M-kaz-ml@aist.go.jp>
2022-01-06 07:35:17 +0900

    Add pypi.sh

---
+++ python/pypi.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# PyPI Registration Script ver. 0.0.0
+#
+# This script requires ​setup.py, setup.cfg, and ~/.pypirc explained in 
+# https://github.com/KazKobara/tips-jp/blob/gh-pages/python/PyPI.md
+# (in Japanese).
+# 
+# Usage:
+# ./pypi.sh <package_name> <package_ver>
+
+### Functions ###
+
+# @brief Read 'y' or 'n' and then return 0 or 1, respectively.
+# @return 0 for 'y' and 1 for 'n'.
+y_or_n () {
+    while read -r ans; do
+	if [ "$ans" == "y" ]; then
+	    return 0
+	elif [ "$ans" == "n" ]; then
+	    return 1
+	fi
+	echo -n "Enter either 'y' or 'n':"
+    done
+}
+
+# @brief Remove files then exit 1.
+# @param $1 package_name
+# @param $2 version
+# @exit 1
+rm_exit1 (){
+    echo "Edit files so that the tests should pass."
+    rm -f dist/"$1"-"$2"{-py*,.tar.gz}
+    exit 1
+}
+
+# @brief Show upload-check message 1.
+# @param $1 package_name
+# @param $2 version
+upload_check_message1 () {
+    echo
+    echo "In a test environment, run the following commands, then test it!"
+    echo " \$ pip uninstall $1"
+    echo " \$ pip install $3 $1==$2"
+}
+
+# @brief Show upload-check message 2.
+# @param $1 package_name
+# @param $2 version
+# @exit 1
+upload_check_message2 () {
+    echo
+    echo -n "Are the tests passed? [y/n]:"
+    y_or_n
+    if [ "$?" == "1" ]; then
+        echo
+        echo "Assign '$1.__version__' a new version number, such as '1.0.0a1',"
+        echo "(since the same version number cannot be uploaded)."
+        rm_exit1 "$1" "$2"
+    fi
+    echo
+}
+
+### Body ###
+
+# Setup and check
+python setup.py sdist bdist_wheel && echo && twine check dist/"$1"-"$2"{-py*,.tar.gz}
+
+echo
+echo -n "Have the checks 'PASSED'? [y/n]:"
+y_or_n
+if [ "$?" == "1" ]; then
+    echo
+    rm_exit1 "$1" "$2"
+fi
+
+# Local test
+upload_check_message1 "$1" "$2" "--no-index --find-links=$(pwd)/dist"
+echo
+echo -n "Are the tests passed? [y/n]:"
+y_or_n
+if [ "$?" == "1" ]; then
+    echo
+    rm_exit1 "$1" "$2"
+fi
+
+echo
+echo "====== Uploading to testpypi ========="
+twine upload -r testpypi dist/"$1"-"$2"{-py*,.tar.gz}
+# Test using testpypi
+upload_check_message1 "$1" "$2" "-i https://test.pypi.org/simple/"
+upload_check_message2 "$1" "$2"
+
+echo
+echo "====== Uploading to pypi ========="
+twine upload -r pypi dist/"$1"-"$2"{-py*,.tar.gz}
+# Test using pypi
+upload_check_message1 "$1" "$2"
+upload_check_message2 "$1" "$2"
+
+echo "Done!!"
+exit 0
